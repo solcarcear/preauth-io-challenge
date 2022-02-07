0878cc51e89de43d75059739248db4b6547f0f28
carce <carlos.3101a@gmail.com>
2022-02-07 16:14:28 -0400

    Game-02 completed

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -9,61 +9,89 @@ export class Item {
         this.quality = quality;
     }
 }
+export class Provider {
+    static readonly normalItems = 'Normal';
+    static readonly conjuredItem = 'Conjured';
+
+    name: string;
+    provide: string;
+
+    constructor(name: string, provide: string) {
+        this.name = name;
+        this.provide = provide;
+    }
+}
+export class ItemToSell extends Item {
+    static readonly itemBackstageName = 'Backstage passes to a TAFKAL80ETC concert';
+    static readonly itemAgedBrieName = 'Aged Brie';
+    static readonly itemSulfuraName = 'Sulfuras, Hand of Ragnaros';
+
+    provider: Provider;
+    factorDegreeQuality: number;
+    expired: boolean;
+    degreeIsPlus: boolean;
+
+    constructor(name: string, sellIn: number, quality: number, provider: Provider) {
+        super(name, sellIn, quality);
+        this.provider = provider;
+        this.factorDegreeQuality = provider.name === Provider.conjuredItem ? 2 : 1;
+        this.expired = sellIn - 1 < 0;
+        this.degreeIsPlus =
+            provider.name === ItemToSell.itemBackstageName || provider.name === ItemToSell.itemBackstageName;
+    }
+}
 
 export class GildedRose {
-    items: Array<Item>;
+    items: Array<ItemToSell>;
 
-    constructor(items = [] as Array<Item>) {
+    constructor(items = [] as Array<ItemToSell>) {
         this.items = items;
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        this.items.map(item => {
+            const sellIn = item.sellIn;
+            if (item.name === ItemToSell.itemSulfuraName) {
+                return false;
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
+            if (sellIn > 0) {
+                item.sellIn--;
             }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
 
+            let factorDegree = GildedRose.getFactorDegree(item);
+            let qualityResult = item.quality + factorDegree;
+
+            if (qualityResult < 0) {
+                item.quality = 0;
+            }
+            if (qualityResult > 50) {
+                item.quality = 50;
+            }             
+        });
         return this.items;
     }
+
+    private static getFactorDegree(item: ItemToSell) {
+        let result = item.degreeIsPlus ? 1 : -1;
+        switch (item.name) {
+            case ItemToSell.itemBackstageName: {
+                if (item.sellIn < 11) {
+                    result++;
+                }
+                if (item.sellIn < 6) {
+                    result++;
+                }
+                break;
+            }
+        }
+        if (item.expired) {
+            GildedRose.degreeTwice(result);
+        }
+        return result * item.factorDegreeQuality;
+    }
+
+    private static degreeTwice(val: number) {
+        val = val * 2;
+    }
+
 }
